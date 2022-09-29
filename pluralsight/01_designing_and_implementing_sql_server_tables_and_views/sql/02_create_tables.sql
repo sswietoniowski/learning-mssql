620800d88ee7fb9520e888f101d65c641461725c
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-29 10:16:32 +0200

    Created 1st version of the DB structure

---
+++ pluralsight/01_designing_and_implementing_sql_server_tables_and_views/sql/02_create_tables.sql
@@ -0,0 +1,73 @@
+USE inventory;
+GO
+
+DROP TABLE IF EXISTS dbo.order_details;
+GO
+
+DROP TABLE IF EXISTS dbo.orders;
+GO
+
+DROP TABLE IF EXISTS dbo.customers;
+GO
+
+DROP TABLE IF EXISTS dbo.products;
+GO
+
+DROP TABLE IF EXISTS dbo.categories;
+GO
+
+CREATE TABLE dbo.categories
+(
+    category_id INT NOT NULL IDENTITY
+        CONSTRAINT PK_categories PRIMARY KEY,
+    category_name NVARCHAR(64) NOT NULL 
+        CONSTRAINT UQ_categories_category_name UNIQUE,
+    category_description NVARCHAR(MAX) NULL
+);
+GO
+
+CREATE TABLE dbo.products
+(
+    product_id INT NOT NULL IDENTITY
+        CONSTRAINT PK_products PRIMARY KEY,
+    product_name NVARCHAR(128) NOT NULL,
+    product_description NVARCHAR(MAX) NULL,
+    product_price DECIMAL NOT NULL 
+        CONSTRAINT CK_products_price_ge_zero CHECK (product_price >= 0),
+    category_id INT NOT NULL 
+        CONSTRAINT FK_products_categories 
+            FOREIGN KEY (category_id) REFERENCES dbo.categories 
+            ON UPDATE CASCADE ON DELETE NO ACTION
+);
+GO
+
+CREATE TABLE dbo.customers
+(
+    customer_id INT NOT NULL IDENTITY
+        CONSTRAINT PK_customers PRIMARY KEY,
+    customer_first_name NVARCHAR(64) NOT NULL,
+    customer_last_name NVARCHAR(128) NOT NULL,    
+);
+GO
+
+CREATE TABLE dbo.orders
+(
+    order_id INT NOT NULL IDENTITY
+        CONSTRAINT PK_orders PRIMARY KEY,
+    order_date DATETIME2 NOT NULL
+        CONSTRAINT DF_orders_order_date DEFAULT (GETDATE())
+);
+GO
+
+CREATE TABLE dbo.order_details
+(
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    product_quantity INT NOT NULL
+        CONSTRAINT CK_order_details_product_quantity_gt_zero CHECK (product_quantity > 0),
+    product_price DECIMAL NOT NULL
+        CONSTRAINT CK_product_price_ge_zero CHECK (product_price >= 0)
+);
+GO
+
+
